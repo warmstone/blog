eac3fd4dd80a1e5d6adc6747b6470a6dabfd8698
pengshun <719732387@qq.com>
2022-04-06 21:40:37 +0800

    add logback-spring.xml

---
+++ blog-web/src/main/java/com/ballcat/blog/BlogApplication.java
@@ -0,0 +1,16 @@
+package com.ballcat.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author pengshun
+ * @date 2022-04-06 21:29
+ * @description
+ */
+@SpringBootApplication
+public class BlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogApplication.class, args);
+    }
+}
