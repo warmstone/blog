30028ad58cb381cc3d337f4e18c4928dbcad6dec
pengshun <719732387@qq.com>
2022-04-07 22:49:44 +0800

    集成Swagger2生成在线接口文档

--- blog-web/src/main/java/com/ballcat/blog/BlogApplication.java
+++ blog-web/src/main/java/com/ballcat/blog/BlogApplication.java
@@ -3,12 +3,14 @@ package com.ballcat.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author pengshun
  * @date 2022-04-06 21:29
  * @description
  */
+@EnableSwagger2
 @SpringBootApplication
 @MapperScan(basePackages = "com.ballcat.blog.mapper")
 public class BlogApplication {
---
+++ blog-web/src/main/java/com/ballcat/blog/config/Swagger2Config.java
@@ -0,0 +1,38 @@
+package com.ballcat.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author pengshun
+ * @date 2022-04-07 22:33
+ * @description
+ */
+@Configuration
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ballcat.blog.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.ballcat.blog.api"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Blog接口文档")
+                .description("一个小而美的博客")
+                .version("1.0")
+                .build();
+    }
+}
