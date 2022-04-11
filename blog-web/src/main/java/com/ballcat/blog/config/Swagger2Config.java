4b8a7f772789835d763f45086f8e0290caafaa29
pengshun <719732387@qq.com>
2022-04-11 13:43:38 +0800

    Swagger2添加全局header

--- blog-web/src/main/java/com/ballcat/blog/config/Swagger2Config.java
+++ blog-web/src/main/java/com/ballcat/blog/config/Swagger2Config.java
@@ -5,9 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author pengshun
@@ -24,7 +28,9 @@ public class Swagger2Config {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ballcat.blog.controller"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts());
     }
 
     private ApiInfo apiInfo() {
@@ -33,5 +39,27 @@ public class Swagger2Config {
                 .description("一个小而美的博客")
                 .version("1.0")
                 .build();
+    }
+
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> schemeList = new ArrayList<>();
+        ApiKey securityScheme = new ApiKey("Authorization", "Authorization", "header");
+        schemeList.add(securityScheme);
+        return schemeList;
+    }
+
+    private List<SecurityContext> securityContexts() {
+        List<SecurityContext> contextList = new ArrayList<>();
+        List<SecurityReference> securityReferences = new ArrayList<>();
+        securityReferences.add(new SecurityReference("Authorization", scopes()));
+        contextList.add(SecurityContext
+                .builder()
+                .securityReferences(securityReferences)
+                .build());
+        return contextList;
+    }
+
+    private AuthorizationScope[] scopes() {
+        return new AuthorizationScope[]{new AuthorizationScope("global", "accessAnything")};
     }
 }
